4f0d90ea93a881be94c0a1e0b6f8b3b61cc5fa36
Mikheev Egor <fromzer@mail.ru>
2020-05-14 13:39:03 +0300

    Spring Boot Application

---
+++ Sweatter/src/main/java/org/example/sweatter/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package org.example.sweatter.repos;
+
+import org.example.sweatter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
