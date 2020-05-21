1c1abecd6c99111c723abd5f263fe066b2c77c99
Mikheev Egor <fromzer@mail.ru>
2020-05-21 12:19:18 +0300

    Spring Boot - bean validation, шифрование паролей

--- Sweatter/src/main/resources/db/migration/V2__Add_admin.sql
+++ Sweatter/src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,8 @@
 insert into users(id, username, password, active)
-    values (1, 'admin', '123', true);
+    values (1, 'admin', '$2y$12$e03wR04ZR2ZnMnVbwQmgi.uA/KPVvm2345/sAV0VwpUfdhpQp8/h.', true);
 
 insert into user_role (user_id, roles)
     values (1, 'USER'), (1, 'ADMIN');
+
+insert into hibernate_sequence(next_val)
+    values (1);
